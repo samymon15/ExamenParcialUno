e73c2fbf4b4b9a1a4aeb292513a8c3ce3cb8dece
samymon15 <samiramontes15@gmail.com>
2020-10-15 15:45:26 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require ("express");
+const exphbs = require ("express-handlebars");
+// Importar body parser que nos permite acceder al cuerpo de la petición HTTP
+const bodyParser = require("body-parser");
+
+
